a2d480a4bb307decfc1386fc1dcea7f780b71c37
Ericka Tuchan <125705781+ErickaT23@users.noreply.github.com>
2025-07-03 11:54:49 -0600

    loads

--- loads.js
+++ loads.js
@@ -1,7 +1,37 @@
-
 const guests = [
-    { id: "1", name: "Marina Estrada", passes: 1, gender: "femenino" }
-];
+    { id: "1", name: "Marina Estrada", passes: 1, gender: "femenino" },
+    { id: "2", name: "Amilcar Jacobo", passes: 1, gender: "masculino" },
+    { id: "3", name: "Amarilis y Cesar", passes: 2, gender: "mixto" },
+    { id: "4", name: "Maria José y Pablo", passes: 2, gender: "mixto" },
+    { id: "5", name: "Marisol y Alison", passes: 2, gender: "femenino" },
+    { id: "6", name: "Bryan Taltique", passes: 3, gender: "masculino" },
+    { id: "7", name: "Salome y Joab", passes: 2, gender: "mixto" },
+    { id: "8", name: "Karla y Gerardo", passes: 2, gender: "mixto" },
+    { id: "9", name: "Sully Lopez", passes: 1, gender: "femenino" },
+    { id: "10", name: "Isidra y Almir", passes: 2, gender: "mixto" },
+    { id: "11", name: "Flor y Juan Carlos", passes: 2, gender: "mixto" },
+    { id: "12", name: "Anthony Aguilar", passes: 1, gender: "masculino" },
+    { id: "13", name: "Edson Aguilar", passes: 1, gender: "masculino" },
+    { id: "14", name: "Lety y José", passes: 2, gender: "mixto" },
+    { id: "15", name: "Norma y Leonel", passes: 2, gender: "mixto" },
+    { id: "16", name: "Olga Estrada", passes: 1, gender: "femenino" },
+    { id: "17", name: "Manuel Yanes", passes: 1, gender: "masculino" },
+    { id: "18", name: "Cony y Rudy", passes: 2, gender: "mixto" },
+    { id: "19", name: "Karla Alonzo", passes: 1, gender: "femenino" },
+    { id: "20", name: "Vivian y Luis Fernando", passes: 2, gender: "mixto" },
+    { id: "21", name: "Ada y Eddy", passes: 2, gender: "mixto" },
+    { id: "22", name: "Rudy y Velveth", passes: 4, gender: "mixto" },
+    { id: "23", name: "Antonio Carrillo", passes: 1, gender: "masculino" },
+    { id: "24", name: "Elvis y Loyda", passes: 2, gender: "mixto" },
+    { id: "25", name: "Emerson", passes: 1, gender: "masculino" },
+    { id: "26", name: "Fernando y Guadalupe", passes: 2, gender: "mixto" },
+    { id: "27", name: "Dylan y Sara", passes: 2, gender: "mixto" },
+    { id: "28", name: "Ricardo y Adela", passes: 2, gender: "mixto" },
+    { id: "29", name: "Anthony Aguirre", passes: 2, gender: "masculino" },
+    { id: "30", name: "Amilcar y Nora", passes: 2, gender: "mixto" },
+    { id: "31", name: "José Quiñonez", passes: 2, gender: "masculino" },
+    { id: "32", name: "Estas invitado", passes: 1, gender: "masculino" }
+  ];
 
 document.addEventListener("DOMContentLoaded", function() {
     function getQueryParams() {
